501f4f354380349c528db73e584c1132daca782a
aloisvandevoorde <36737105+Aloisvdvoorde@users.noreply.github.com>
2019-02-28 19:27:30 +0100

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -9,27 +9,35 @@ def parse_input(file):
     return list
 
 
-def getNumberOfPhotos(list):
+def getNumber(list):
     number = int((list.pop(0))[0])
     return number
 
 
 def sortLists(list):
-    for i in range(0,len(list)):
+    for i in range(0,len(list)-1): # je mag niet meer als 1 keer dit doen
         tup = [i] + list[i]
+        tup[2] = int(float(tup[2]))
+        print(tup)
         if list[i][0] == 'H':
             H.append(tup)
         elif list[i][0] == 'V':
             V.append(tup)
 
 
+def getHorizontalPhotos():
+    return H
 
+
+def getVerticalPhotos():
+    return V
+
+
+def getNumberOfPhotos():
+    return n
 
 
 l = parse_input(f)
 n = getNumberOfPhotos(l)
 sortLists(l)
-print('h:')
-print(H)
-print('v:')
-print(V)
+
